4ffaf6ef80b22b7d5d1234018b240a274cfc576a
Zenthm <zenthm.dev@gmail.com>
2024-01-26 15:09:35 +0800

    Update build backend

--- backend/build.py
+++ backend/build.py
@@ -3,51 +3,44 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Any
 
+from setuptools import Command
 from setuptools.command.build_ext import build_ext as _build_ext
-from setuptools.extension import Extension
 
 
-class CMakeExtension(Extension):
-    def __init__(self: CMakeExtension) -> None:
-        super().__init__("cmake", sources=[])
+class cmake(Command):
+    def initialize_options(self: cmake) -> None:
+        pass
 
+    def finalize_options(self: cmake) -> None:
+        pass
 
-class build_ext(_build_ext):
-    def build_extension(self: build_ext, ext: Extension) -> None:
-        if isinstance(ext, CMakeExtension):
-            self.build_cmake()
-            self.extensions.remove(ext)
-            return
-        super().build_extension(ext)
+    def run(self: cmake) -> None:
+        build_ext = self.get_finalized_command("build_ext")
+        build_py = self.get_finalized_command("build_py")
 
-    def build_cmake(self: build_ext) -> None:
-        cwd = Path().absolute()
-        root_dir = str(cwd)
-        build_dir = str(cwd / "build")
+        self.spawn(["cmake", "-S", ".", "-B", "build"])
+        self.spawn(["cmake", "--build", "build"])
 
-        self.spawn(["cmake", "-S", root_dir, "-B", build_dir])
-        self.spawn(["cmake", "--build", build_dir])
-
-        if self.inplace:
-            build_py = self.get_finalized_command("build_py")
+        if build_ext.inplace:
             package_dir = Path(build_py.get_package_dir("pyndow"))
         else:
-            package_dir = Path(self.build_lib) / "pyndow"
+            package_dir = Path(build_ext.build_lib) / "pyndow"
+            package_dir.mkdir(parents=True, exist_ok=True)
 
-        ext_path = cwd / "build" / "ext"
-        exts = ext_path.rglob("*")
-
-        for ext in exts:
+        for ext in Path("build/ext").rglob("*"):
             ext.rename(package_dir / ext.name)
 
 
-ext_modules = [CMakeExtension()]
+class build_ext(_build_ext):
+    def run(self: build_ext) -> None:
+        self.run_command("cmake")
+        super().run()
 
 
 def pdm_build_update_setup_kwargs(_, kwargs: dict[str, Any]) -> None:
     kwargs.update(
-        ext_modules=ext_modules,
         cmdclass={
             "build_ext": build_ext,
+            "cmake": cmake,
         },
     )
