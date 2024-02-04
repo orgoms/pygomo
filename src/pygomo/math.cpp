1e639623eceb78e02561f9f30db76b51e3a31e12
Zenthm <zenthm.dev@gmail.com>
2024-02-04 14:43:52 +0800

    Add Point3D class

--- src/pygomo/math.cpp
+++ src/pygomo/math.cpp
@@ -21,6 +21,12 @@ public:
     double x, y;
 };
 
+class Point3D {
+public:
+    Point3D(double x = 0.0, double y = 0.0, double z = 0.0) : x(x), y(y), z(z) {}
+    double x, y, z;
+};
+
 PYBIND11_MODULE(math, mod) {
     mod.doc() = "Math module for pygomo";
     py::class_<Vector2D>(mod, "Vector2D")
@@ -36,4 +42,9 @@ PYBIND11_MODULE(math, mod) {
         .def(py::init<double, double>(), "x"_a = 0.0, "y"_a = 0.0)
         .def_readwrite("x", &Point2D::x)
         .def_readwrite("y", &Point2D::y);
+    py::class_<Point3D>(mod, "Point3D")
+        .def(py::init<double, double, double>(), "x"_a = 0.0, "y"_a = 0.0, "z"_a = 0.0)
+        .def_readwrite("x", &Point3D::x)
+        .def_readwrite("y", &Point3D::y)
+        .def_readwrite("z", &Point3D::z);
 }
