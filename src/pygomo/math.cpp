40d0591956676ffb7f074f9734861ac494555b9b
Zenthm <zenthm.dev@gmail.com>
2024-02-04 14:42:26 +0800

    Add Vector3D class

--- src/pygomo/math.cpp
+++ src/pygomo/math.cpp
@@ -9,10 +9,21 @@ public:
     double x, y;
 };
 
+class Vector3D {
+public:
+    Vector3D(double x = 0.0, double y = 0.0, double z = 0.0) : x(x), y(y), z(z) {}
+    double x, y, z;
+};
+
 PYBIND11_MODULE(math, mod) {
     mod.doc() = "Math module for pygomo";
     py::class_<Vector2D>(mod, "Vector2D")
         .def(py::init<double, double>(), "x"_a = 0.0, "y"_a = 0.0)
         .def_readwrite("x", &Vector2D::x)
         .def_readwrite("y", &Vector2D::y);
+    py::class_<Vector3D>(mod, "Vector3D")
+        .def(py::init<double, double, double>(), "x"_a = 0.0, "y"_a = 0.0, "z"_a = 0.0)
+        .def_readwrite("x", &Vector3D::x)
+        .def_readwrite("y", &Vector3D::y)
+        .def_readwrite("z", &Vector3D::z);
 }
