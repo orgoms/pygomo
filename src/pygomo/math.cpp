ee8a592a288081d672c03b46f6acd880306ebdb1
Zenthm <zenthm.dev@gmail.com>
2024-02-04 11:59:48 +0800

    Add Vector2D class

--- src/pygomo/math.cpp
+++ src/pygomo/math.cpp
@@ -1,7 +1,18 @@
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
+using namespace pybind11::literals;
+
+class Vector2D {
+public:
+    Vector2D(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+    double x, y;
+};
 
 PYBIND11_MODULE(math, mod) {
     mod.doc() = "Math module for pygomo";
+    py::class_<Vector2D>(mod, "Vector2D")
+        .def(py::init<double, double>(), "x"_a = 0.0, "y"_a = 0.0)
+        .def_readwrite("x", &Vector2D::x)
+        .def_readwrite("y", &Vector2D::y);
 }
