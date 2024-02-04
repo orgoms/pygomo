b675036861af0659f70f305d18539105dad3b2b0
Zenthm <zenthm.dev@gmail.com>
2024-02-04 15:45:03 +0800

    Add Clock class

--- src/pygomo/time.cpp
+++ src/pygomo/time.cpp
@@ -1,5 +1,13 @@
 #include <pybind11/pybind11.h>
 
+namespace py = pybind11;
+
+class Clock {
+public:
+    Clock() {}
+};
+
 PYBIND11_MODULE(time, mod) {
     mod.doc() = "Time module for pygomo.";
+    py::class_<Clock>(mod, "Clock");
 }
