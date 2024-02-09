3844e05ed7a1ce91b84ec5cd432eef7e458dbda7
Zenthm <zenthm.dev@gmail.com>
2024-02-09 16:13:48 +0800

    Add init function

--- src/pygomo/core.cpp
+++ src/pygomo/core.cpp
@@ -1,5 +1,16 @@
+#define GLFW_INCLUDE_NONE
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <pybind11/pybind11.h>
+
+void init() {
+    glfwInit();
+    gladLoadGL();
+}
 
 PYBIND11_MODULE(core, mod) {
     mod.doc() = "Core module for pygomo.";
+    mod.def("init", &init);
 }
