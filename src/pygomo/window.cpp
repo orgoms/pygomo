27d2e890001ae0fd33b89154e25aa90cadb6585e
Zenthm <zenthm.dev@gmail.com>
2024-02-09 16:30:56 +0800

    Add Window class

--- src/pygomo/window.cpp
+++ src/pygomo/window.cpp
@@ -1,5 +1,13 @@
 #include <pybind11/pybind11.h>
 
+namespace py = pybind11;
+
+class Window {
+public:
+    Window() {}
+};
+
 PYBIND11_MODULE(window, mod) {
     mod.doc() = "Window module for pygomo.";
+    py::class_<Window>(mod, "Window");
 }
