e48cf47c4d773aab9e7c7c6839ab82192d1299a6
Zenthm <zenthm.dev@gmail.com>
2024-02-09 16:50:10 +0800

    Add Window parameters

--- src/pygomo/window.cpp
+++ src/pygomo/window.cpp
@@ -2,12 +2,14 @@
 #include <GLFW/glfw3.h>
 #include <pybind11/pybind11.h>
 
+#include <string>
+
 namespace py = pybind11;
 
 class Window {
 public:
-    Window() {
-        this->window = glfwCreateWindow(640, 480, "Window", NULL, NULL);
+    Window(std::string title = "Window", int width = 640, int height = 480) {
+        this->window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
     }
 
     void destroy() {
