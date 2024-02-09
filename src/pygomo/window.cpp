682a9d330ac5c3946594ab8aeb9ce26582a68494
Zenthm <zenthm.dev@gmail.com>
2024-02-09 16:40:42 +0800

    Add Window constructor

--- src/pygomo/window.cpp
+++ src/pygomo/window.cpp
@@ -1,13 +1,22 @@
+#define GLFW_INCLUDED_NONE
+
+#include <GLFW/glfw3.h>
+
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
 class Window {
 public:
-    Window() {}
+    Window() {
+        this->window = glfwCreateWindow(640, 480, "Window", NULL, NULL);
+    }
+
+    GLFWwindow *window;
 };
 
 PYBIND11_MODULE(window, mod) {
     mod.doc() = "Window module for pygomo.";
-    py::class_<Window>(mod, "Window");
+    py::class_<Window>(mod, "Window")
+        .def(py::init<>());
 }
